a07edc2a9e9d93d8a0cfdc3ca6755596f5d3f33d
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-10-18 13:00:19 +0500

    proj

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
+// Initialize Supabase
+let SUPABASE_URL = "https://lntrfndfszbbhycarfjt.supabase.co";
+let SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxudHJmbmRmc3piYmh5Y2FyZmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1MjQ1MDMsImV4cCI6MjA3MjEwMDUwM30.qrr-rlmqvBgoa76u5rXl8vMqpJozQkqpGyzXSwNfMzo";
+let supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// -----------<<< Authentication [Signup & Login] Section >>>--------------
+
+// Elements
 let userEmail = document.getElementById("userEmail")
 let userPassword = document.getElementById("userPassword")
 let forgetPassw = document.getElementById("forgetPassw")
 
-userEmail.addEventListener("change" , () => {
-    console.log(userEmail.value);
-})
+let signupBtn = document.getElementById("signupBtn")
+let loginBtn = document.getElementById("loginBtn")
 
-userPassword.addEventListener("change" , () => {
-    console.log(userPassword.value);
-})
 
-forgetPassw.addEventListener("click" , () => {
-    console.log("click");
-})
-
-// Icon Handling
+// Show & Hide Password Handling
 changIcon.addEventListener("click" , () => {
 
     if (changIcon.classList.contains("fa-eye")) {
@@ -27,3 +27,126 @@ changIcon.addEventListener("click" , () => {
     }
     
 })
+
+// Sigup User Handling
+signupBtn.addEventListener("click" , async () => {
+
+    const { data, error } = await supabase.auth.signUp({
+        email: userEmail.value,
+        password: userPassword.value
+    })
+
+    if (error) {
+        error.message
+    }
+    else {
+        showToast("Confirm Your Email!" , "A confirmation link has been sent to your email address. Please check your inbox to verify your account before logging in." , "emerald-500")
+        console.log(data);
+    }
+
+    userEmail.value = ""
+    userPassword.value = ""
+})
+
+// Login User Handling
+loginBtn.addEventListener("click" , async () => {
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: userEmail.value,
+        password: userPassword.value
+    })
+
+    if (error) {
+        showToast("Invalid Credentials!" , "The email or password you entered is incorrect. Please double-check your details and try again." , "red-500")
+    }
+    else {
+        showToast("Login Successful!" , "You have successfully logged in to your account. Welcome back!" , "purple-500")
+        location.href = "./main.html"
+        console.log(data);
+    }
+
+    userEmail.value = ""
+    userPassword.value = ""
+})
+
+
+// -----------<<< Main Page Section >>>--------------
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------<<< Popup Notification Handling >>>--------------
+
+// Elements
+let toastDiv = document.getElementById("toastDiv")
+let signinGoogle = document.getElementById("signinGoogle")
+
+// user show Notification
+function showToast(message , descrp , color) {
+
+    toastDiv.innerHTML +=
+    `
+    <div class="absolute inset-0 flex items-center justify-center backdrop-blur-sm bg-black/40 z-50">
+        <div role="alert" class="mb-6 mx-auto max-w-lg rounded-lg bg-stone-100 p-4 shadow-lg sm:p-6 lg:p-8">
+        
+            <div class="flex items-center gap-4">
+                <span class="shrink-0 rounded-full bg-${color} p-2 text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4">
+                    <path fill-rule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" clip-rule="evenodd"></path>
+                </svg>
+                </span>
+                <p class="font-medium sm:text-lg text-${color}">${message}</p>
+            </div>
+
+            <p class="mt-4 text-gray-600">${descrp}</p>
+
+            <div class="mt-6 sm:flex sm:gap-4">
+                <a href="#" onclick="console.log(this.parentElement.parentElement.parentElement.remove())" class="inline-block w-full rounded-lg bg-${color} px-5 py-3 text-center text-sm font-semibold text-white sm:w-auto">
+                ok</a>
+                <a href="#" onclick="console.log(this.parentElement.parentElement.parentElement.remove())" class="mt-2 inline-block w-full rounded-lg bg-stone-200 px-5 py-3 text-center text-sm font-semibold text-gray-800 sm:mt-0 sm:w-auto hover:bg-stone-300">
+                Dismiss</a>
+            </div>
+
+        </div>
+    </div>
+    `
+
+}
+
+signinGoogle.addEventListener("click" , () => {
+    showToast("Feature Locked!" , "This feature is currently locked — it will be unlocked in a future update!" , "purple-700")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function getData() {
+//     const { data, error } = await supabase.from('Hackathon-Project').select()
+
+//     if (data) {
+//         console.log("✅ Data fetched successfully:", data);
+//     }
+
+//     if (error) {
+//         console.error("❌ Error fetching data:", error.message);
+//     }
+// }
+// getData();
